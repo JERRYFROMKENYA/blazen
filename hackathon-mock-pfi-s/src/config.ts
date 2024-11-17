68e757018f478297cd0e7ceb2efdc540772ed5c1
jerry <jerryochieng178@gmail.com>
2024-11-17 14:17:04 +0300

    Mobile: Extern Pfi: New tbdex comm: none

---
+++ hackathon-mock-pfi-s/src/config.ts
@@ -0,0 +1,88 @@
+import type { LogLevelDesc } from 'loglevel'
+import fs from 'fs/promises'
+
+import 'dotenv/config'
+
+import { BearerDid, DidDht } from '@web5/dids'
+
+
+export type Environment = 'local' | 'staging' | 'production'
+const pfi_path = 'src/pfis/'
+
+export type Config = {
+  env: Environment;
+  logLevel: LogLevelDesc;
+  host: string[];
+  port: string[];
+  pfiDid: BearerDid[];
+  issuerDid: BearerDid;
+  allowlist: string[];
+  pinPaymentsKey: string;
+}
+
+export const config: Config = {
+  env: (process.env['ENV'] as Environment) || 'local',
+  logLevel: (process.env['LOG_LEVEL'] as LogLevelDesc) || 'info',
+  host: [ 'http://localhost:6000',  'http://localhost:5000', 'http://localhost:8000',  'http://localhost:8080', 'http://localhost:9090'],
+  port: ['6000', '5000', '8000', '8080', '9090'],
+  pfiDid: await createOrLoadDid(['pfi_1.json', 'pfi_2.json', 'pfi_3.json', 'pfi_4.json', 'pfi_issuer.json']),
+  issuerDid: await createADid('https://mock-idv.tbddev.org', 'issuer.json'),
+  pinPaymentsKey: process.env['SEC_PIN_PAYMENTS_SECRET_KEY'],
+  allowlist: JSON.parse(process.env['SEC_ALLOWLISTED_DIDS'] || '[]'),
+}
+
+async function loadDID(filename) {
+  try {
+    const data = await fs.readFile(pfi_path + filename, 'utf-8')
+    return JSON.parse(data)
+  } catch (error) {
+    console.error('Error reading from file:', error)
+    return false
+  }
+}
+
+async function createADid(serviceEndpoint: string, filename: string) {
+  const existingDid = await loadDID(filename)
+  if (existingDid) {
+    const bearerDid = await DidDht.import({portableDid: existingDid})
+    return bearerDid
+  }
+  // else create a new one
+  const bearerDid = await DidDht.create({
+    options: {
+      services: [
+        {
+          id: 'pfi',
+          type: 'PFI',
+          serviceEndpoint,
+        },
+      ],
+    },
+  })
+  const portableDid = await bearerDid.export()
+  await fs.writeFile(pfi_path + filename, JSON.stringify(portableDid, null, 2))
+
+  return bearerDid
+}
+
+
+
+async function createOrLoadDid(filenames: string[]): Promise<BearerDid[]> {
+
+  console.log('Setting up dids for server PFIs and Issuer...')
+  const pfis: BearerDid[] = []
+  const bearerDid1 = await createADid('https://aqf-mock-pfis.tbddev.org', filenames[0])
+  const bearerDid2 = await createADid('https://sls-mock-pfis.tbddev.org', filenames[1])
+  const bearerDid3 = await createADid('https://ff-mock-pfis.tbddev.org', filenames[2])
+  const bearerDid4 = await createADid('https://vla-mock-pfis.tbddev.org', filenames[3])
+  const bearerDid5 = await createADid('https://tt-mock-pfis.tbddev.org', filenames[4])
+
+
+  pfis.push(bearerDid1)
+  pfis.push(bearerDid2)
+  pfis.push(bearerDid3)
+  pfis.push(bearerDid4)
+  pfis.push(bearerDid5)
+
+  return pfis
+}
---
+++ hackathon-mock-pfi-s/src/offerings.ts
@@ -0,0 +1,110 @@
+import { OfferingsApi, Offering } from '@tbdex/http-server'
+import { config } from './config.js'
+// import fs from 'fs/promises'
+import { PresentationExchange } from '@web5/credentials'
+import { BearerDid } from '@web5/dids'
+
+import * as offerings from './offeringData.js'
+
+
+
+// Offerings order
+/*
+1. African currency exchanges PFI
+2. Another African currency exchanges PFI
+3. USD related exchanges PFI
+4. EUR related exchanges PFI
+5. High liquidity exchanges PFI
+*/
+
+const customOfferings = [
+  // first set of offerings per PFI
+  {data: {...offerings.offeringDataGHSToUSDC}}, // PFI 1
+  {data: {...offerings.offeringDataKESToUSD2}}, // PFI 2
+  {data: {...offerings.offeringDataUSDToEUR}}, // PFI 3
+  {data: {...offerings.offeringDataEURToUSD2}}, // PFI 4
+  {data: {...offerings.offeringDataUSDToAUD}}, // PFI 5
+
+  // second set of offerings per PFI
+  {data: {...offerings.offeringDataNGNToKES}},
+  {data: {...offerings.offeringDataKESToUSDC}},
+  {data: {...offerings.offeringDataEURToUSD}},
+  {data: {...offerings.offeringDataEURToUSDC}},
+  {data: {...offerings.offeringDataUSDToGBP2}},
+
+  // third set of offerings per PFI
+  {data: {...offerings.offeringDataKESToUSD}},
+  {data: {...offerings.offeringDataNGNToGHS}},
+  {data: {...offerings.offeringDataUSDToGBP}},
+  {data: {...offerings.offeringDataUSDToEUR2}},
+  {data: {...offerings.offeringDataUSDToKES2}},
+
+  // fourth set of offerings per PFI
+  {data: {...offerings.offeringDataUSDToKES}},
+  {data: {...offerings.offeringDataBTCToNGN}},
+  {data: {...offerings.offeringDataUSDToBTC}},
+  {data: {...offerings.offeringDataEURToGBP}},
+  {data: {...offerings.offeringDataUSDToMXN}},
+]
+
+// Function to create a randomized offering
+async function createRandomOffering(index: number): Promise<Offering> {
+
+  const customPFIIndex = index % 5 // 5 is number of hardcoded PFI DIDs
+  const offering = Offering.create({
+    metadata: {
+      from: config.pfiDid[customPFIIndex].uri,  // Alternates between 5 PFI URIs
+      protocol: '1.0'
+    },
+    data: customOfferings[index].data //chooseRandomOfferingData(customPFIIndex),
+  })
+
+  try {
+    await offering.sign(config.pfiDid[customPFIIndex])
+    // console.log('Offering signed')
+  }
+  catch (e) {
+    console.log('error', e)
+  }
+  // offering.sign(config.pfiDid[index % 5])  // Sign with alternating URI
+
+  try {
+    offering.validate()
+    PresentationExchange.validateDefinition({
+      presentationDefinition: offering.data.requiredClaims
+    })
+  } catch (error) {
+    console.error(`Offering ${index + 1} failed validation: ${error}`)
+  }
+
+
+  // console.log(`Offering ${index + 1} created and validated`)
+  return offering
+}
+
+// Initialize an array of hardcoded offerings
+const hardcodedOfferings: Offering[] = await Promise.all(Array.from({ length: 20 }, (_, i) => createRandomOffering(i)))
+
+export class HardcodedOfferingRepository implements OfferingsApi {
+  pfi: BearerDid
+  pfiHardcodedOfferings: Offering[]
+
+  constructor(pfi: BearerDid) {
+    this.pfi = pfi
+    this.pfiHardcodedOfferings = hardcodedOfferings.filter((offering) => offering.metadata.from === pfi.uri)
+  }
+  // Retrieve a single offering if found
+  async getOffering(opts: { id: string }): Promise<Offering | undefined> {
+    console.log('call for offerings')
+    return this.pfiHardcodedOfferings.find((offering) => offering.id === opts.id)
+  }
+
+  // Retrieve a list of offerings
+  async getOfferings(): Promise<Offering[] | undefined> {
+    console.log('get PFI offerings')
+    return this.pfiHardcodedOfferings
+  }
+}
+
+// Export an instance of the repository
+// export const OfferingRepository = new HardcodedOfferingRepository()
